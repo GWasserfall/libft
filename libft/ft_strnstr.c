59f1ad23753c1aee5cd4f910ab94982d9ebacba0
Event WeThinkCode <event@c3r1s6.wethinkcode.co.za>
2019-05-15 14:35:46 +0200

    adding str funcs

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+char	*ft_strstr(char *haystack, char *needle)
+{
+	int len;
+	int index;
+
+	index = 0;
+	len = ft_strlen(needle);
+
+	if (len == 0)
+		return (haystack);
+	while (*haystack++)
+	{
+		while (needle[index] != 0)
+		{
+			if (needle[index] != *(haystack + index))
+				break;
+			if (index == len - 1)
+				return haystack;
+			index++;
+		}
+		index = 0;
+	}
+	return NULL;
+}
