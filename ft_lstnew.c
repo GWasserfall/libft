6cf6c2758f2d27073e0c98a637eadadd1fa6ecf1
Glen Wasserfall <glen@wasserfalls.co.za>
2019-05-28 23:17:26 +0200

    ft_lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -5,8 +5,18 @@ t_list *ft_lstnew(void const *content, size_t content_size)
 	t_list *list;
 
 	list = (t_list *)malloc(sizeof(t_list));
-
 	if (!list)
 		return (NULL);
+	if (!content)
+	{
+		list->content = NULL;
+		list->content_size = 0;
+	}
+	else
+	{
+		list->content = content;
+		list->content_size = content_size;
+	}
+	list->next = NULL;
 	return (list);
 }
