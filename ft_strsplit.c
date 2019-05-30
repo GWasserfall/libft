ce89bee55d0bee1be34dd5d9af4aa766de0fa558
Glen WASSERFALL <gwasserf@c3r3s6.wethinkcode.co.za>
2019-05-30 11:34:48 +0200

    completed 1 & 2

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -1,16 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwasserf <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/30 11:15:26 by gwasserf          #+#    #+#             */
+/*   Updated: 2019/05/30 11:16:04 by gwasserf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	**ft_strsplit(char const *s, char c)
 {
-	char **array;
-	int words;
-	int str_index;
-	int arr_index;
+	char	**array;
+	int		words;
+	int		str_index;
+	int		arr_index;
 
-	printf("Got str: [%s]\n",s);
 	arr_index = 0;
 	words = ft_wordcount((char *)s, c);
-	printf("words: [%d]\n",words);
 	array = malloc((sizeof(char *) * (words + 1)));
 	while (*s)
 	{
@@ -18,48 +28,14 @@ char	**ft_strsplit(char const *s, char c)
 		if (*s != c && *s != '\0')
 		{
 			while (s[str_index] != c && s[str_index] != '\0')
-			{	
-				printf("[%d] : [%c]\n", str_index, s[str_index]);
 				str_index++;
-			}
 			array[arr_index] = ft_strnew(str_index + 1);
-			printf("passed to memcopy : %s\n", s);
 			ft_memcpy(array[arr_index], s, str_index);
-			s += str_index;
+			s += str_index - 1;
 			arr_index++;
 		}
 		s++;
 	}
-	array[words + 1] = NULL;
-	return(array);
+	array[words] = NULL;
+	return (array);
 }
-
-
-// void	ft_print_result(char const *s)
-// {
-// 	int		len;
-
-// 	len = 0;
-// 	while (s[len])
-// 		len++;
-// 	write(1, s, len);
-// }
-
-// int main()
-// {
-
-// 	char	**tabstr;
-// 	int		i;
-
-// 	i = 0;
-// 	tabstr = ft_strsplit("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse", ' ');
-// 	while (tabstr[i])
-// 	{
-// 		ft_print_result(tabstr[i]);
-// 		write(1, "\n", 1);
-// 		i++;
-// 	}
-
-
-// 	return (0);
-// }
--- ft_wordcount.c
+++ ft_wordcount.c
@@ -1,13 +1,25 @@
-int ft_wordcount(const char *str, char delim)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_wordcount.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwasserf <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/30 11:16:23 by gwasserf          #+#    #+#             */
+/*   Updated: 2019/05/30 11:33:29 by gwasserf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_wordcount(const char *str, char delim)
 {
 	int counter;
 
-	counter = 0; 
+	counter = 0;
 	while (*str)
 	{
-		if (*str!= delim)
+		if (*str != delim)
 		{
-			while((*str != delim) && *str)
+			while ((*str != delim) && *str)
 				str++;
 			counter++;
 		}
