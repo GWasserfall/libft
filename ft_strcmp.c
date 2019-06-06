b019e3def14e070fae841098e7b5c07b390754a2
Glen WASSERFALL <glen@wasserfalls.co.za>
2019-06-06 13:01:36 +0200

    moving macs

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,16 +6,25 @@
 /*   By: gwasserf <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 18:29:52 by gwasserf          #+#    #+#             */
-/*   Updated: 2019/05/28 11:03:35 by gwasserf         ###   ########.fr       */
+/*   Updated: 2019/06/05 18:13:16 by gwasserf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strcmp(const char *s1, const char *s2)
+#include "libft.h"
+
+int		ft_strcmp(const char *str1, const char *str2)
 {
-	while (*s1 && (*s1 == *s2))
+	size_t i;
+
+	i = 0;
+	while ((str1[i] != '\0') || (str2[i] != '\0'))
 	{
-		s1++;
-		s2++;
+		if ((unsigned char)str1[i] > (unsigned char)str2[i])
+			return (1);
+		if ((unsigned char)str1[i] < (unsigned char)str2[i])
+			return (-1);
+		i++;
 	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
+	return (0);
 }
+
